031b8cb127d9c4cee64123d552adb6ee3f1987fa
matthew t muniz-silva <mmunizsi01@gmail.com>
2018-12-11 00:55:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+function getFirstSelector(selector){
+  var first = document.querySelector(selector);
+  return first
+}
 
+function nestedTarget(){
+  var first = document.getElementById("nested").querySelector(".target")
+  return first
+}
+
+function increaseRankBy(n){
+  const lis = document.querySelectorAll('ul.ranked-list li');
+   
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML++
+}
+return lis
+}
+function deepestChild(){
+  document.getElementById("grand-node").querySelector("div");
+}
